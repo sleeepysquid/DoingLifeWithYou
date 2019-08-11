6f7cafc057a2b5ed8e3647282e80f641e211deb1
Lawrence <lawmbass1@gmail.com>
2019-08-11 14:23:26 -0400

    Adjust width and placement of embedded blog video

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -29,8 +29,8 @@ const options = {
           <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
             <iframe
               title={fields.title["en-US"]}
-              width="560"
-              height="315"
+              width="100%"
+              height="400"
               src={fields.url["en-US"]}
               frameBorder="0"
               allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
