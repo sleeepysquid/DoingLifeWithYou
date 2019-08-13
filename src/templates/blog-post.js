db9c1eb98be534dfb9732a9b685cabe42cc5f76e
Lawrence <lawmbass1@gmail.com>
2019-08-12 21:09:39 -0400

    Add comment section to blog posts

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -9,11 +9,13 @@ import heroStyles from '../components/hero.module.css'
 import blogStyles from './blog-post.module.css'
 import SignUpForm from '../components/signup-form'
 
-import { BLOCKS, MARKS } from "@contentful/rich-text-types"
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import { BLOCKS, MARKS } from '@contentful/rich-text-types'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 
-const Bold = ({ children }) => <span className="bold"> {children}</span>
-const Text = ({ children }) => <p className="align-center">{children}</p>
+import { DiscussionEmbed } from 'disqus-react'
+
+const Bold = ({ children }) => <span className='bold'> {children}</span>
+const Text = ({ children }) => <p className='align-center'>{children}</p>
 const EmbeddedImage = ({ children }) => <img className={blogStyles.embeddedImg} src={`${children.url}?fm=jpg&q=25`} />
 
 const options = {
@@ -22,18 +24,18 @@ const options = {
   },
   renderNode: {
     [BLOCKS.PARAGRAPH]: (node, children) => <Text>{children}</Text>,
-    [BLOCKS.EMBEDDED_ASSET]: ({ data: { target: { fields }}}) => <EmbeddedImage>{fields.file["en-US"]}</EmbeddedImage>,
+    [BLOCKS.EMBEDDED_ASSET]: ({ data: { target: { fields }}}) => <EmbeddedImage>{fields.file['en-US']}</EmbeddedImage>,
     [BLOCKS.EMBEDDED_ENTRY]: ({ data: { target: { fields, sys: { contentType: { sys: { id }}} }}}) => {
       if (id === 'video') {
         return (
           <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
             <iframe
-              title={fields.title["en-US"]}
-              width="100%"
-              height="400"
-              src={fields.url["en-US"]}
-              frameBorder="0"
-              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              title={fields.title['en-US']}
+              width='100%'
+              height='400'
+              src={fields.url['en-US']}
+              frameBorder='0'
+              allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
               allowFullScreen>
             </iframe>
           </div>
@@ -41,9 +43,9 @@ const options = {
       }
       if (id === 'map') {
         return <iframe
-          src={fields.url["en-US"]}
-          width="640"
-          height="480">
+          src={fields.url['en-US']}
+          width='640'
+          height='480'>
         </iframe>
       }
 
@@ -57,6 +59,14 @@ class BlogPostTemplate extends React.Component {
   render() {
     const post = get(this.props, 'data.contentfulBlogPost')
     const siteTitle = get(this.props, 'data.site.siteMetadata.title')
+    const disqusConfig = {
+      shortname: process.env.GATSBY_DISQUS_NAME,
+      config: {
+        url: `https://doinglifewithyou/blog/${post.slug}`,
+        identifier: post.slug,
+        title: post.title 
+      },
+     }
 
     return (
       <Layout location={this.props.location} >
@@ -64,11 +74,11 @@ class BlogPostTemplate extends React.Component {
           <Helmet title={`${post.title} | ${siteTitle}`} />
           <div className={heroStyles.hero}>
             <Img className={heroStyles.heroImage} alt={post.title} fluid={post.heroImage.fluid} />
-            <div className="gradient-layer"></div>
+            <div className='gradient-layer'></div>
           </div>
           <div className={`wrapper ${blogStyles.grid}`}>
-            <div className="blog-content">
-              <h1 className="section-headline">{post.title}</h1>
+            <div className='blog-content'>
+              <h1 className='section-headline'>{post.title}</h1>
               <p
                 style={{
                   display: 'block',
@@ -78,8 +88,10 @@ class BlogPostTemplate extends React.Component {
               </p>
 
               { post.content && documentToReactComponents(post.content.json, options) }
+
+              <DiscussionEmbed {...disqusConfig} />
             </div>
-            <div className="sidebar">
+            <div className='sidebar'>
                 <SignUpForm />
             </div>
           </div>
@@ -100,6 +112,7 @@ export const pageQuery = graphql`
     }
     contentfulBlogPost(slug: { eq: $slug }) {
       title
+      slug
       publishDate(formatString: "MMMM Do, YYYY")
       heroImage {
         fluid {
