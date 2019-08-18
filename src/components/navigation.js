7ca2ae6ee820959d1daaa37259b9ada14537d55c
Lawrence <lawmbass1@gmail.com>
2019-08-18 19:38:41 -0400

    Add responsive navbar

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 import styles from './navigation.module.css'
 import { ReactTypeformEmbed } from 'react-typeform-embed';
+import { FaBars } from 'react-icons/fa';
 
 class Navigation extends React.Component {
   constructor(props) {
@@ -11,6 +12,19 @@ class Navigation extends React.Component {
 
   openForm() {
     this.typeformEmbed.typeform.open();
+  }
+
+  openNavbar() {
+    var mainNav = document.getElementById("mainNav");
+    var logo = document.getElementById("logo");
+
+    if (mainNav.className === styles.navbar) {
+      mainNav.className += ` ${styles.responsive}`;
+      logo.className += ` ${styles.hideLogo}`
+    } else {
+      mainNav.className = styles.navbar;
+      logo.className = styles.logo;
+    }
   }
 
   render() {
@@ -24,21 +38,18 @@ class Navigation extends React.Component {
             this.typeformEmbed = tf;
           }}
         />
-        <nav role="navigation" className={styles.navbar}>
-          <Link to="/">
+        <nav role="navigation" className={styles.navbar} id="mainNav">
+          <Link className={styles.logo} to="/" id="logo">
             <img src="/images/notexture_logo.png" style={{ maxWidth: 150, marginTop: '-20px' }}/>
           </Link>
-          <ul className={styles.navList}>
-            <li className={styles.navItem}>
-              <Link to="/blog/">Blog</Link>
-            </li>
-            <li className={styles.navItem}>
-              <a href="https://www.youtube.com/channel/UC41zd0suaVa-UD0C6iK285Q" target="_blank" rel="noopener">YouTube</a>
-            </li>
-            <li className={styles.navItem} onClick={this.openForm}>
-              Contact
-            </li>
-          </ul>
+          <div className={styles.navList}>
+              <Link className={styles.navItem} to="/blog/">Blog</Link>
+              <a className={styles.navItem} href="https://www.youtube.com/channel/UC41zd0suaVa-UD0C6iK285Q" target="_blank" rel="noopener">YouTube</a>
+              <a className={styles.navItem} href="javascript:void(0);" onClick={this.openForm}>Contact</a>
+              <a className={`${styles.navItem} ${styles.navToggle}` } href="javascript:void(0);" onClick={this.openNavbar}>
+                <FaBars size="25" />
+              </a>
+          </div>
         </nav>
       </div>
     )
